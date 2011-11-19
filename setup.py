bc92a80adf005ab94375e7be36133a2c6c57d386
Gagandeep Singh <gagandeep.2020@gmail.com>
2011-11-19 19:49:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
  
 setup(
-    name='facebook-sdk',
+    name='python-sdk',
     version='0.2.1',
     description='This client library is designed to support the Facebook Graph API with Oauth 2.0 and the official Facebook JavaScript SDK, which is the canonical way to implement Facebook authentication.',
     author='Facebook',
